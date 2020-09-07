d20be59adee36097c95ae96fc4b2492163a93b58
mtodorov95 <mtodorov95@gmail.com>
2020-09-07 09:25:32 +0300

    db connection

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import Message from "./models/Message.js";
+
+// ENV variables
+dotenv.config();
 
 // App config
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware
+app.use(express.json());
+
+// DB config
+mongoose.connect(process.env.MONGO_URL, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 // Api routes
 app.get("/", (req, res) => {
   res.status(200).send("Hello!");
+});
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Message.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(`New message created: \n ${data}`);
+    }
+  });
 });
 
 // Listen
